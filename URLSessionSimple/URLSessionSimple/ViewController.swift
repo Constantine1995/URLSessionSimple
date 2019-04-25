135e1c601a0958943f035cb7c63aec3a86fc69d0
Kostya Likhachev <arrow.koss@gmail.com>
2019-04-25 12:47:53 +0300

    - initial commit

---
+++ URLSessionSimple/URLSessionSimple/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  URLSessionSimple
+//
+//  Created by mac on 4/25/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
